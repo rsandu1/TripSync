574c01ee86b542298cd95847dc48fd5d083a53e2
rsandu1 <robert.sandu@eclosetapp.com>
2026-04-26 16:15:17 -0400

    Update Index.ts

--- api/index.ts
+++ api/index.ts
@@ -3,6 +3,8 @@ import { createApp } from "../server/app.ts";
 const appPromise = createApp().then(({ app }) => app);
 
 export default async function handler(req: any, res: any) {
-  const app = await appPromise;
+  const mod = await import("../server/app.ts");
+  const { app } = await mod.createApp();
+
   return app(req, res);
 }
